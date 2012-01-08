6a24ad204af5bd17a97cd2cec241c24653c1fdfa
Ignat Kolesnichenko <ignat@yandex-team.ru>
2012-01-08 05:09:04 +0400

    Add processing exit codes

--- util.py
+++ util.py
@@ -5,7 +5,10 @@ def run(command, return_stdout=False):
     print "RUNNING:", command
     pipe = subprocess.PIPE if return_stdout else None
     proc = subprocess.Popen(command, shell=True, stdout=pipe)
-    return proc.communicate()[0]
+    stdout = proc.communicate()[0]
+    if proc.returncode != 0:
+        raise Exception("suprocess interrupted with error: %d" % proc.returncode)
+    return stdout
 
 def remove_file(filename):
     print "Removing file", filename
